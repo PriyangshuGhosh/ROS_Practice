b861f2657926ff0520db450fd993918e256343f2
Priyangshu Ghosh <priyangshu0906@gmail.com>
2025-12-13 11:56:54 +0530

    commiting

--- ros2_ws/build/my_2_pkgcpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/darkknight/ros2_ws/src/my_2_pkgcpp
-# Build directory: /home/darkknight/ros2_ws/build/my_2_pkgcpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
